60d0fe264cecc00b93f3dd92c24476b5c0715f67
sundarimsec <sundari@msec.edu.in>
2026-06-01 00:21:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Calculator Application Running"
+    if request.method == "POST":
+        a = int(request.form["num1"])
+        b = int(request.form["num2"])
+        result = a + b
+
+        return f"""
+        <h1>Calculator Result</h1>
+        <h2>{a} + {b} = {result}</h2>
+        <a href="/">Back</a>
+        """
+
+    return """
+    <h1>Simple Calculator</h1>
+    <form method="POST">
+        Number 1: <input type="number" name="num1"><br><br>
+        Number 2: <input type="number" name="num2"><br><br>
+        <input type="submit" value="Add">
+    </form>
+    """
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run()
